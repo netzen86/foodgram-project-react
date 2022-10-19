cb40690c2a4dcd3116fa19dc040692f42e064783
netzen86 <netzen0su@gmail.com>
2022-10-20 00:06:50 +0500

    Написал функционал заполнения таблицы Ingredients

---
+++ backend/foodgram/management/commands/fill_db.py
@@ -0,0 +1,22 @@
+import csv
+import os
+
+from django.conf import settings as set
+from django.core.management.base import BaseCommand
+from foodgram.models import Ingredients
+
+DATA_DIR = os.path.join(set.BASE_DIR, '..', "data")
+
+
+class Command(BaseCommand):
+    help = 'Заполнение БД'
+
+    def handle(self, *args, **options):
+        with open(f'{DATA_DIR}/ingredients.csv', 'r', encoding='utf8') as f:
+            dr = csv.reader(f, delimiter=',')
+            for row in dr:
+                Ingredients.objects.get_or_create(
+                    name=row[0],
+                    measurement_unit=row[1]
+                )
+            self.stdout.write('Таблица Ingredients заполнена!')
