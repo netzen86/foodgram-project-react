ef7fe674633dd0e21def1e830fa88bfdf982bbb7
netzen86 <netzen0su@gmail.com>
2022-10-27 01:15:12 +0500

    изменил создание админского акка

--- backend/foodgram/management/commands/createadmin.py
+++ backend/foodgram/management/commands/createadmin.py
@@ -7,15 +7,11 @@ from users.models import CustomUser as User
 class Command(BaseCommand):
     def handle(self, *args, **options):
         if User.objects.count() == 0:
-            admin = User.objects.get_or_create(
+            print('Создаём админский акаунт')
+            User.objects.create_superuser(
                 username=os.getenv('BE_USERNAME', default='admin'),
-                first_name=os.getenv('BE_FNAME', default='Zavulon'),
-                last_name=os.getenv('BE_LNAME', default='Ivanovich'),
                 email=os.getenv('BE_EMAIL', default='admin@yandex.ru'),
                 password=os.getenv('BE_PASSWORD', default='p@s$w0rd777'),
-                is_subscribed=False,
-                role='admin'
             )
-            admin.save()
         else:
             print('Админский акаунт уже создан')
