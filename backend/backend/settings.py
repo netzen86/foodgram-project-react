788fe8cf78260118f807408ccce52c0f36efac79
netzen86 <netzen0su@gmail.com>
2023-04-04 22:48:34 +0500

    Удалил лишние комментарии.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -5,10 +5,6 @@ from django.core.management.utils import get_random_secret_key
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = get_random_secret_key()
 
